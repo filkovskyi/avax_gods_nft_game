6b844b3b6c9e6a589206b32456c3b39590f77896
Sergey Filkovskyi <filkovskyi@gmail.com>
2023-03-22 15:03:35 +0200

    initial commit

---
+++ client/src/page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-5xl p-3">Avax Gods</h1>
+      <h2 className="text-3xl p-3">Web3 NFT Battle-style Card Game</h2>
+    </div>
+  )
+};
+
+export default Home;
